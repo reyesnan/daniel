ce0c62457e0af12eed6f84b49ba802cd4fc563dd
reyesnan <reyesnan@sheridancollege.ca>
2022-02-07 17:37:18 -0300

    prueba12

--- src/javaapplication18/JavaApplication18.java
+++ src/javaapplication18/JavaApplication18.java
@@ -16,6 +16,9 @@ public class JavaApplication18 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("");
+        System.out.println("");
     }
 //lkjgfdfj
 }
